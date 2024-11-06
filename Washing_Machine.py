e93d0613456f56df00dec6eba34d97045ffe8d25
Ghantasala Sai Lakshmi Prasanna <sai027139@gmail.com>
2024-11-06 16:06:18 +0530

    Add files via upload

---
+++ Washing_Machine.py
@@ -0,0 +1,21 @@
+import time
+
+def washing_cloths():
+    print("Washing Machine is started!")
+    print("It stat the washing my cloths")
+    time.sleep(3)
+    print("Hey it washed successfully")
+
+    print("It stat the rinsed my cloths")
+    time.sleep(2)
+    print("Hey it rinsed successfully")
+
+    print("It stat the spin my cloths")
+    time.sleep(1)
+    print("Hey it spin successfully")
+
+    print('successfully done')
+
+washing_cloths()
+
+
